e65cbc9aecaf7c251d310cec83c19d1de65231a1
PhilemonVanDussen <vandussenph709@northwested.org>
2026-03-10 12:51:57 -0400

    update 1

---
+++ main.js
@@ -0,0 +1,14 @@
+// PJ VanDussen
+// 09 March 2026
+// Variable Declaration & Assignment
+
+// Declare variables
+const firstName = "PJ";
+const lastName = "VanDussen";
+const age = 25;
+
+
+// Show value of each variable in the browser console
+
+console.log(firstName);
+console.log(lastName);
