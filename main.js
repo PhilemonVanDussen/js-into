40fb12254795b7601fec7382b98cb4df9ee25e24
PhilemonVanDussen <vandussenph709@northwested.org>
2026-03-11 07:52:08 -0400

    Updated main.js

--- main.js
+++ main.js
@@ -7,9 +7,19 @@ const firstName = 'PJ';
 const lastName = 'VanDussen';
 const age = 25;
 
+const mySchool = 'North Ed Career Tech';
+const gradLevel = '12'
+
+const eyeColor = 'hazel'
+
 
 // Show value of each variable in the browser console
 
 console.log(firstName);
 console.log(lastName);
 console.log(age);
+
+console.log(mySchool);
+console.log(gradLevel);
+
+console.log(eyeColor);
---
+++ report-card.js
@@ -0,0 +1,28 @@
+// PJ VanDussen
+// 10 Mar 2026
+// JS Report Card
+
+// Declare variables
+const firstName = 'Joe';
+const lastName = 'Rogan';
+const gradeLevel = 11;
+const course = 'Chemistry'
+
+const quiz1 = 43;
+const quiz2 = 55;
+const quiz3 = 11;
+
+// Write formula to do the math
+const totalQuiz = 3
+const totalPoints = quiz1 + quiz2 + quiz3;
+const avgPoints = totalPoints / totalQuiz;
+
+console.log('First Name:', firstName);
+console.log('Last Name:', lastName);
+console.log(gradeLevel, 'Grade');
+console.log('Class:', course);
+console.log('Quiz 1:', quiz1)
+console.log('Quiz 2:', quiz2)
+console.log('Quiz 3:', quiz3)
+console.log('Grade score:', avgPoints);
+
